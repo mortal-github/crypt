4ec0be476990395767bfc89a7f0ce4ac1984d688
mortal-github <13710503031@139.com>
2020-11-04 18:00:29 +0800

    add method to encryp plaintext to XOR class

--- src/mortal/learn/gdut/crypt/xor/XOR.java
+++ src/mortal/learn/gdut/crypt/xor/XOR.java
@@ -10,4 +10,15 @@ public class XOR extends JFrame {
         Objects.requireNonNull(key);
         this.key = key.getBytes();
     }
+
+    public byte[] encrypt(byte[] plaintext){
+        byte[] ciphertext = new byte[plaintext.length];
+        for(int i=0, j=0; i<plaintext.length; i++){
+            ciphertext[i] = (byte) (this.key[j] ^ plaintext[i]);
+            j++;
+            if(key.length == j)
+                j = 0;
+        }
+        return ciphertext;
+    }
 }
